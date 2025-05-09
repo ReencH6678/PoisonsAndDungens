8a16c5fb8b014fcd84873649468cb5cff94f8643
ReencH6678 <ddaammiirr6678@gmail.com>
2025-05-09 15:42:44 +0200

    Make counter

--- Assets/Scripts/Counter.cs
+++ Assets/Scripts/Counter.cs
@@ -1,48 +1,49 @@
+using System;
 using System.Collections;
-using TMPro;
 using UnityEngine;
 
 public class Counter : MonoBehaviour
 {
+    public event Action<float> VelueChendeg;
+
     [SerializeField] private float _deley;
     [SerializeField, Min(0)] private float _startCount;
 
-    [SerializeField] private TextMeshProUGUI _text;
-
     private float _currentCount;
-    private bool _isOn = true;
+    private bool _isOn = false;
 
     private void Start()
     {
         _currentCount = _startCount;
-        StartCoroutine(Countup(_deley));
     }
 
     private void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            _isOn = !_isOn;
+
+            if (_isOn)
+            {
+                StartCoroutine(Countup(_deley));
+            }
+            else
+            {
+                StopCoroutine(Countup(_deley));
+            }
+
+        }
     }
 
     private IEnumerator Countup(float deley)
     {
-        var wait = new WaitForSecondsRealtime(deley);
-
         while (_isOn)
         {
-            if (Input.GetMouseButton(0))
-            {
-                _isOn = !_isOn;
-            }
+            var wait = new WaitForSecondsRealtime(deley);
 
-            _currentCount++;
-            DisplayCountup(_currentCount);
+            VelueChendeg?.Invoke(_currentCount++);
 
             yield return wait;
         }
-    }
-
-    private void DisplayCountup(float count)
-    {
-        _text.text = count.ToString("");
     }
 }
--- Assets/Scripts/CounterView.cs
+++ Assets/Scripts/CounterView.cs
@@ -1,8 +1,24 @@
-using System.Collections;
+using TMPro;
 using UnityEngine;
 
+[RequireComponent(typeof(Counter))]
 public class CounterView : MonoBehaviour
 {
     [SerializeField] private Counter _counter;
+    [SerializeField] private TextMeshProUGUI _text;
 
+    private void OnEnable()
+    {
+        _counter.VelueChendeg += DisplayCountup;
+    }
+
+    private void OnDisable()
+    {
+        _counter.VelueChendeg -= DisplayCountup;
+    }
+
+    private void DisplayCountup(float count)
+    {
+        _text.text = count.ToString("");
+    }
 }
